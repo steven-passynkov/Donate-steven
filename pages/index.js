4f6b36ab92b19d4d9a74d6197616c4e91d1b1d90
Steven Passynkov <stevenpassynkov@flame01.domain.com>
2021-02-08 11:40:35 -0500

    Donate-steven

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import products from '../products.json'
+import {initiateCheckout} from '../lib/payments.js'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,24 +14,28 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Steven's <a href="">donation site</a>
+          Welcome to Steven's donation site
         </h1>
 
         <div className={styles.grid}>
-          <button>Donate now</button>
+          {products.map(product =>{
+          const {title,id} = product;
+          return(
+          <p key={id}>
+            <button className={styles.button} onClick={() => {
+                      initiateCheckout({
+                        lineItems: [
+                          {
+                            price:id,
+                            quantity:1
+                          }
+                        ]
+                      })
+                    }}>{title}</button>
+          </p>
+            )})}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
